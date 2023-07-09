197689f15498d658d9d3e030136a4b4af58ccdc3
FajarAprido <fajaraprido29@gmail.com>
2023-07-09 15:13:07 +0700

    add peminjman

--- database/migrations/2023_07_09_072019_create_peminjmen_table.php
+++ database/migrations/2023_07_09_072019_create_peminjmen_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('peminjman', function (Blueprint $table) {
+        Schema::create('peminjmen', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->string('buku');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('peminjman');
+        Schema::dropIfExists('peminjmen');
     }
 };
