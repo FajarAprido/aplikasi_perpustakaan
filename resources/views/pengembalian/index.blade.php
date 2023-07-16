73d90e55f0ecf4b6632958150971acb80ca36d11
kumaratofani10 <kumaratofani41@gmail.com>
2023-07-16 12:36:04 +0700

    index dan delete pengembalian

--- resources/views/pengembalian/index.blade.php
+++ resources/views/pengembalian/index.blade.php
@@ -15,7 +15,7 @@ Managemen Pengembalian
             <table class="table">
               <thead>
                 <tr>
-                  <th scope="col">Card ID</th>
+                  <th scope="col">ID</th>
                   <th scope="col">Nama</th>
                   <th scope="col">Buku</th>
                   <th scope="col">No Hp</th>
@@ -27,15 +27,14 @@ Managemen Pengembalian
                 @foreach ($dataPengembalian as $index => $pengembalian)
                 <tr>
                   <th scope="row">{{ ++$index }}</th>
-                  <td>{{ $pengembalian->cardId }}</td>
                   <td>{{ $pengembalian->nama }}</td>
                   <td>{{ $pengembalian->buku }}</td>
-                  <td>{{ $pengembalian->noHp }}</td>
-                  <td>{{ $pengembalian->tanggalPengembalian }}</td>
+                  <td>{{ $pengembalian->NoHp }}</td>
+                  <td>{{ $pengembalian->tanggalPengmbalian }}</td>
                   <td>
-                    <a class="dropdown-item" href="{{ route('pengembalian.edit',$buku->id) }}">
+                    <!-- <a class="dropdown-item" href="{{ route('pengembalian.edit',$pengembalian->id) }}">
                         <i class="bx bx-edit-alt me-1"></i> Edit
-                    </a >  
+                    </a >   -->
                     <form action="{{ route('pengembalian.destroy',$pengembalian->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
@@ -44,34 +43,6 @@ Managemen Pengembalian
                   </td>
                 </tr>
                 @endforeach
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Bridie Kessler</td>
-                  <td>Developer</td>
-                  <td>35</td>
-                  <td>2014-12-05</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Ashleigh Langosh</td>
-                  <td>Finance</td>
-                  <td>45</td>
-                  <td>2011-08-12</td>
-                </tr>
-                <tr>
-                  <th scope="row">4</th>
-                  <td>Angus Grady</td>
-                  <td>HR</td>
-                  <td>34</td>
-                  <td>2012-06-11</td>
-                </tr>
-                <tr>
-                  <th scope="row">5</th>
-                  <td>Raheem Lehner</td>
-                  <td>Dynamic Division Officer</td>
-                  <td>47</td>
-                  <td>2011-04-19</td>
-                </tr>
               </tbody>
             </table>
             <!-- End Default Table Example -->
