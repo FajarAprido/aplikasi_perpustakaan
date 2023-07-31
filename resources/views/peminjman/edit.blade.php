ba54294ee8b2592f3b3e05cc86eda93b3ba606ec
FajarAprido <fajaraprido29@gmail.com>
2023-07-31 21:06:25 +0700

    change edit peminjaman data

--- resources/views/peminjman/edit.blade.php
+++ resources/views/peminjman/edit.blade.php
@@ -17,25 +17,25 @@ Managemen Waktu Peminjaman
                 <div class="row mb-3 mt-3">
                   <label for="inputText" class="col-sm-2 col-form-label">Nama</label>
                   <div class="col-sm-10">
-                    <input value="{{ $peminjman->judul }}" name="judul" type="text" class="form-control">
+                    <input value="{{ $peminjman->nama }}" name="nama" type="text" class="form-control">
                   </div>
                 </div>
                 <div class="row mb-3">
                   <label for="inputText" class="col-sm-2 col-form-label">buku</label>
                   <div class="col-sm-10">
-                    <input value="{{ $peminjman->pengarang }}" name="pengarang" type="text" class="form-control">
+                    <input value="{{ $peminjman->buku }}" name="buku" type="text" class="form-control">
                   </div>
                 </div>
                 <div class="row mb-3">
                   <label for="inputText" class="col-sm-2 col-form-label">NoHp</label>
                   <div class="col-sm-10">
-                    <input value="{{ $peminjman->penerbit }}" name="penerbit" type="text" class="form-control">
+                    <input value="{{ $peminjman->NoHp }}" name="NoHp" type="text" class="form-control">
                   </div>
                 </div>
                 <div class="row mb-3">
                   <label for="inputText" class="col-sm-2 col-form-label">Tanggal</label>
                   <div class="col-sm-10">
-                    <input value="{{ $peminjman->tahun }}" name="tahun" type="text" class="form-control">
+                    <input value="{{ $peminjman->tanggalPinjam }}" name="tanggalPinjam" type="text" class="form-control">
                   </div>
                 </div>
 
